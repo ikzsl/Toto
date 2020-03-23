3f112d2641c91263b6fc070651c344981e2bde27
dmm <ikzsl@yandex.ru>
2020-03-23 13:09:31 +0300

    Updates

---
+++ precache-manifest.27f5ce3e869cefc1d1d14ee002dc5ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437cf9ebe35645d145253e093ac617b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20a9a85c5b18f5b05701",
+    "url": "/static/css/main.9569bf34.chunk.css"
+  },
+  {
+    "revision": "d7dc44b912418a642715",
+    "url": "/static/js/2.6d369c36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d369c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a9a85c5b18f5b05701",
+    "url": "/static/js/main.1d04388c.chunk.js"
+  },
+  {
+    "revision": "5f349a93d8e0cf4cfe67",
+    "url": "/static/js/runtime-main.ad38e011.js"
+  }
+]);
